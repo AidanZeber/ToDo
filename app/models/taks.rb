dc0cfe521f5f9afb237eb83ed3cbddb826ebe6fd
Aidan Zebertavage <aidanzeber@gmail.com>
2016-06-25 15:00:41 +0000

    set up factory girl for tests and index controller for tasks

--- app/models/taks.rb
+++
@@ -1,2 +0,0 @@
-class Taks < ActiveRecord::Base
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615034653) do
+ActiveRecord::Schema.define(version: 20160625142858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "taks", force: true do |t|
+    t.string   "title"
+    t.boolean  "done",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "tasks", force: true do |t|
     t.string   "title"
     t.boolean  "done",       default: false
     t.datetime "created_at",                 null: false
